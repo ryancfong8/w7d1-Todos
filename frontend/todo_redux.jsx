5ca4c655e4dce4e947546aea2d8e08a7762c879d
App Academy Student <student@appacademy.io>
2017-03-06 10:55:16 -0800

    First Commit

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import store from './store';
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<h1>Todos App</h1>, document.getElementById('root'));
+});
